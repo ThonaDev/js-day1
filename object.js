c6dfa68391154617369f1b92ba4c82e857d3f12e
ThonaDev <thonaben08@gmail.com>
2026-04-29 16:11:41 +0700

    do not use arrow function in Object

--- object.js
+++ object.js
@@ -6,5 +6,16 @@ const userInfo = {
     username: "Dudu",
     age: 21,
     email: "dudu168@gmail.com",
-    isActive: true
+    isActive: true,
+    address: {
+        province: "Kampot",
+        country: "Cambodia"
+    },
+    greeting: function(){
+        console.log("Hello, World!");
+        console.log(`Welcome, ${this.username}`);
+    }
 }
+
+console.log(userInfo);
+userInfo.greeting();
